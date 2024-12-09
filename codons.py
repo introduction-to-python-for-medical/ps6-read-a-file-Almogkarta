874167446c55b2c86a0d7d33a18e05251e76e7eb
Almogkarta <karta.almog@gmail.com>
2024-12-09 15:56:44 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,10 +1,13 @@
 def create_codon_dict(file_path):
     amino_acid_dict = {} 
+    with open(file_path, 'r') as file:
+        rows = file.readlines()
     for row in rows:
         row_cells = row.strip().split('\t')
         if len(row_cells) >= 3:
           codon = row_cells[0]
           amino_acid = row_cells[2]
           amino_acid_dict[codon] = amino_acid
+    return amino_acid_dict
 
 
